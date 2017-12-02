6ac7520a45ceb920a705101ef79779972060e442
balazs4 <balazs4web@gmail.com>
2017-12-02 17:28:28 +0100

    feat(app): introduce basic structure

--- windowswitcher.app/App.xaml.cs
+++ windowswitcher.app/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using Unity;
 
 namespace windowswitcher.app
 {
@@ -13,5 +15,57 @@ namespace windowswitcher.app
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            IUnityContainer ioc = new UnityContainer();
+            ioc.RegisterType<IDispatcherService, DispatcherService>();
+            ioc.RegisterType<IWindowSwitcher, MockWindowSwticher>();
+            ioc.RegisterInstance(Current.Dispatcher);
+            var appwindow = new MainWindow
+            {
+                DataContext = ioc.Resolve<AppViewModel>()
+            };
+            appwindow.Show();
+        }
+    }
+
+    public interface IDispatcherService
+    {
+        void Run(Action action);
+    }
+
+    public class DispatcherService : IDispatcherService
+    {
+        private Dispatcher dispatcher { get; }
+        public DispatcherService(Dispatcher _dispatcher)
+        {
+            dispatcher = _dispatcher;
+        }
+
+        public void Run(Action action)
+        {
+            dispatcher.Invoke(action, DispatcherPriority.Normal);
+        }
+    }
+
+
+    public class MockWindowSwticher : IWindowSwitcher
+    {
+        public void ActivateWindow(IWindow window_in) => MessageBox.Show("Activate " + window_in.Title);
+        
+        public IEnumerable<IWindow> GetWindowsUnderCurrentDesktop() => throw new NotImplementedException();
+
+        public IEnumerable<IWindow> QueryWindows() => Enumerable.Range(0, 16).Select(MockWindow.Create);
+    }
+
+    public class MockWindow : IWindow
+    {
+        public static IWindow Create(int number) => new MockWindow { Title = number.ToString() };
+        
+        public string Title { get; private set; }
+
+        public string Desktop { get; private set; }
     }
 }
---
+++ windowswitcher.app/AppViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using windowswitcher;
+
+namespace windowswitcher.app
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            if (PropertyChanged == null) return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class AppViewModel : ViewModel
+    {
+        private readonly IDispatcherService dispatcher;
+        private readonly IWindowSwitcher switcher;
+
+        public AppViewModel(IDispatcherService _dispatcher, IWindowSwitcher _switcher)
+        {
+            windows = new ObservableCollection<IWindow>();
+            dispatcher = _dispatcher;
+            switcher = _switcher;
+            Title = "windowswitcher";
+            Activate = new LambdaCommand(window => switcher.ActivateWindow((IWindow)window), window => window is IWindow);
+        }
+
+        public string Title { get; private set; }
+
+        private string searchText;
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+                RaisePropertyChangedEvent("SearchText");
+            }
+        }
+
+        private ObservableCollection<IWindow> windows;
+        public ObservableCollection<IWindow> Windows
+        {
+            get { return windows; }
+            set
+            {
+                windows = value;
+                RaisePropertyChangedEvent("Candidates");
+            }
+        }
+
+        public ICommand Activate { get; private set; }
+    }
+}
+
