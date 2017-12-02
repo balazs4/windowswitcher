fc5f59104a9407b91d865f302f19f499c2856380
Matthias <Matthias@DESKTOP-OLAFCHA>
2017-12-02 16:42:07 +0100

    Project for Iterfaces and core logic

---
+++ windowswitcher.app/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace windowswitcher
+{
+    public class Class1
+    {
+    }
+}
---
+++ windowswitcher/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace windowswitcher
+{
+    public interface IWindow
+    {
+        string Title { get; }
+
+        string Desktop { get; }
+    }
+
+
+    public interface IWindowSwitcher
+    {
+        IEnumerable<IWindow> GetWindowsUnderCurrentDesktop();
+
+        IEnumerable<IWindow> QueryWindows();
+
+        void ActivateWindow(IWindow window_in);
+
+    }
+
+}
